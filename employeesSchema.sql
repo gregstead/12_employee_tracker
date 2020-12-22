0013b00785a54c6c239d7cbc23d6cd0e63cb4c28
Greg Stead <gregstead@gmail.com>
2020-12-21 18:52:32 -0600

    Database schema working

---
+++ employeesSchema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departments(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE roles(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    dept_id INTEGER,
+    FOREIGN KEY (dept_id) REFERENCES departments(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id),
+    PRIMARY KEY (id)
+);
