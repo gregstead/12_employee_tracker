e01fca4f70b38fc2840bff5adac2bf4586e16af1
Greg Stead <gregstead@gmail.com>
2020-12-26 15:28:59 -0600

    Added Ascii art title for fun

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Dependencies
 // ~~~~~
-
+const titleArt = require("./assets/ascii_title").titleArt;
 const prompts = require("./utils/prompts");
 const connection = require("./utils/connection").connection;
+
+console.log(titleArt);
 
 connection.connect((err) => {
   if (err) throw err;
@@ -11,6 +13,4 @@ connection.connect((err) => {
 
 // Bonus points if you're able to:
 // Update employee managers
-// View employees by manager
-// Delete departments, roles, and employees
 // View the total utilized budget of a department -- ie the combined salaries of all employees in that department
